a7853299251b86e89a665c2dcca464d99e4587d8
georgehadjiantonis@hotmail.com <georgehadjiantonis@hotmail.com>
2019-12-29 15:54:04 +0200

    remove existing dot files and replace them with symlinks, add command to install Vundle vim-plugin-manager

--- install-scripts/Linux/install-packages.sh
+++ install-scripts/Linux/install-packages.sh
@@ -1,1 +1,5 @@
 cat packages.list | xargs sudo apt-get -y install
+
+# install vundle vim plugin-manager
+# Reference: https://github.com/VundleVim/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
