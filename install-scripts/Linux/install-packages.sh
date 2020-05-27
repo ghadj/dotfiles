c8d98662ce95260b0846dca4db2581b46e5fd91f
georgehadjiantonis@hotmail.com <georgehadjiantonis@hotmail.com>
2020-05-27 17:38:37 +0300

    removed - replaced by setup.sh

--- install-scripts/Linux/install-packages.sh
+++
@@ -1,5 +0,0 @@
-cat packages.list | xargs sudo apt-get -y install
-
-# install vundle vim plugin-manager
-# Reference: https://github.com/VundleVim/Vundle.vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
