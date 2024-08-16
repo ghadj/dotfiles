317e7a41a5196cc1904588625d567bfbc0d73c04
George Hadjiantonis <georgehadjiantonis@hotmail.com>
2024-08-17 02:07:37 +0300

    Cleanup

--- scripts/xmodmap.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/zsh
-# remap right ctrl key to space key
-xmodmap -e 'keysym Control_R = KP_Space'
-# remap Ü to backspace
-xmodmap -e 'keysym 0xfc = BackSpace'
-# remap Ä to enter
-xmodmap -e 'keysym 0xe4 = Return'
-# remap Ö to F10
-xmodmap -e 'keysym 0xf6 = F10'
